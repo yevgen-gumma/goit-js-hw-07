f658168ab5aab2610b53c2ce018dd7612b9af364
yevgen-gumma <203040eg@gmail.com>
2024-01-02 20:04:14 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,12 @@
-function isEnoughCapacity(products, containerSize) {
-  let allProducts = 0;
-  let arrayProducts = Object.values(products);
-  for (const oneProduct of arrayProducts) {
-    allProducts += oneProduct;
-  }
-  return allProducts <= containerSize ? true : false;
-}
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
-console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
+const allCategories = document.getElementById('categories');
+
+const items = allCategories.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach(element => {
+  const categoryName = element.firstElementChild.textContent;
+  console.log(`Category: ${categoryName}`);
+  const countOfElements = element.querySelectorAll('ul li:not(.item)').length;
+  console.log(`Elements: ${countOfElements}`);
+});
