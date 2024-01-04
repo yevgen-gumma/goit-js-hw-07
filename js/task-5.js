01064846e245581962805be051d1739a45c9def8
yevgen-gumma <203040eg@gmail.com>
2024-01-04 17:15:07 +0200

    4

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector('.widget');
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+changeColorButton.addEventListener('click', function () {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+
+  colorSpan.textContent = randomColor;
+});
